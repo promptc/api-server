7d744e9d71522584ace087bcc20f36835a12a004
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-05-05 21:27:17 +0100

    example

--- _example/go.mod
+++ _example/go.mod
@@ -1,3 +1,5 @@
 module example
 
 go 1.20
+
+require github.com/promptc/api-server v0.0.1 // indirect
--- _example/main.go
+++ _example/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/KevinZonda/GoX/pkg/iox"
+	"github.com/gin-gonic/gin"
+	"github.com/promptc/api-server/api"
+	scheduler "github.com/promptc/openai-scheduler"
+)
+
 func main() {
 
+	// Load config
+	cfgStr := e(iox.ReadAllText("config.json"))
+	var cfg cfgModel
+	if _e := json.Unmarshal([]byte(cfgStr), &cfg); _e != nil {
+		panic(_e)
+	}
+
+	// Init Prompt API Provider
+	openaiScheduler := scheduler.NewScheduler(cfg.Tokens)
+	apiProvider := api.NewProvider(openaiScheduler, []string{
+		"echo.promptc",
+		"hello.promptc",
+	})
+
+	// Init Gin HTTP Server
+	engine := gin.New()
+	group := engine.Group("/v1")
+
+	// Append API Router
+	// /v1/echo -> echo.promptc
+	// /v1/hi   -> hello.promptc (project: hi)
+	// /v1/hi/stream   -> hello.promptc (project: hi, stream: true)
+	// /v1/echo/stream -> echo.promptc  (stream: true)
+	// /v1/echo/info   -> echo.promptc
+	// /v1/hi/info     -> hello.promptc (project: hi)
+	apiProvider.AppendRouter(group)
+
+	// Start HTTP Server
+	err := engine.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+type cfgModel struct {
+	Tokens []string `json:"tokens"`
+}
+
+func e[T any](i T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
